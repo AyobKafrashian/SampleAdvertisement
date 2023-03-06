d5a949f626f332fd3d50aa9574adec7c522f545e
AyobKafrashian <ayobkafrashian@gmail.com>
2023-03-06 17:41:36 +0330

    add two site for crawler

--- SampleAdvertisement.Api/SampleAdvertisement.Api/Controllers/HomeController.cs
+++ SampleAdvertisement.Api/SampleAdvertisement.Api/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using SampleAdvertisement.Api.Settings;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -15,17 +16,26 @@ namespace SampleAdvertisement.Api.Controllers
         #region Constructor
         private readonly IConfiguration _config;
         private readonly ICrawleringService _crawler;
-        private readonly string _siteAddress;
-        private readonly string _htmlTag;
-        private readonly string _htmlClassName;
+        private readonly CrawlingDivarSettings _settingDivarUrls;
+        private readonly CrawlingKilidSettings _settingKilidUrls;
+        private readonly CrawlingShabeshSettings _settingShabeshUrls;
 
         public HomeController(IConfiguration config, ICrawleringService crawler)
         {
             _config = config;
             _crawler = crawler;
-            _siteAddress = _config.GetValue<string>("CrawlingAddress:Divar:SiteAddress");
-            _htmlTag = _config.GetValue<string>("CrawlingAddress:Divar:HtmlTag");
-            _htmlClassName = _config.GetValue<string>("CrawlingAddress:Divar:HtmlClassName");
+
+            var crawlingDivarSettings = new CrawlingDivarSettings();
+            _config.GetSection("CrawlingAddress:Divar").Bind(crawlingDivarSettings);
+            _settingDivarUrls = crawlingDivarSettings;
+
+            var crawlingKilidSettings = new CrawlingKilidSettings();
+            _config.GetSection("CrawlingAddress:Kilid").Bind(crawlingKilidSettings);
+            _settingKilidUrls = crawlingKilidSettings;
+
+            var crawlingShabeshSettings = new CrawlingShabeshSettings();
+            _config.GetSection("CrawlingAddress:Shabesh").Bind(crawlingShabeshSettings);
+            _settingShabeshUrls = crawlingShabeshSettings;
         }
         #endregion
 
@@ -34,10 +44,40 @@ namespace SampleAdvertisement.Api.Controllers
         {
             try
             {
-                Uri urlReq = new Uri(this._siteAddress);
+                Uri urlDivarReq = new Uri(this._settingDivarUrls.SiteAddress);
+                List<DataCrawlerResponse> result = await _crawler.DivarSiteCrawl(urlDivarReq, this._settingDivarUrls.HtmlTag, this._settingDivarUrls.HtmlClassName);
 
-                List<DivarDataResponse> result = await _crawler.DivarCrawl(urlReq, this._htmlTag, this._htmlClassName);
+                return Ok(new { data = result, statusCode = 200 });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { data = "", statusCode = 500 });
+            }
+        }
 
+        [HttpGet("GetKilidData")]
+        public async Task<IActionResult> GetKilidData()
+        {
+            try
+            {
+                Uri urlKilidReq = new Uri(this._settingKilidUrls.SiteAddress);
+                List<DataCrawlerResponse> result = await _crawler.KilidSiteCrawl(urlKilidReq, this._settingKilidUrls.HtmlTag, this._settingKilidUrls.HtmlClassName);
+
+                return Ok(new { data = result, statusCode = 200 });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { data = "", statusCode = 500 });
+            }
+        }
+
+        [HttpGet("GetShabeshData")]
+        public async Task<IActionResult> GetShabeshData()
+        {
+            try
+            {
+                Uri urlShabeshReq = new Uri(this._settingShabeshUrls.SiteAddress);
+                List<DataCrawlerResponse> result = await _crawler.ShabeshSiteCrawl(urlShabeshReq, this._settingShabeshUrls.HtmlTag, this._settingShabeshUrls.HtmlClassName);
 
                 return Ok(new { data = result, statusCode = 200 });
             }
---
+++ SampleAdvertisement.Api/SampleAdvertisement.Api/Settings/CrawlingSetting.cs
@@ -0,0 +1,21 @@
+﻿namespace SampleAdvertisement.Api.Settings
+{
+    public class CrawlingDivarSettings
+    {
+        public string SiteAddress { get; set; }
+        public string HtmlTag { get; set; }
+        public string HtmlClassName { get; set; }
+    }
+    public class CrawlingKilidSettings
+    {
+        public string SiteAddress { get; set; }
+        public string HtmlTag { get; set; }
+        public string HtmlClassName { get; set; }
+    }
+    public class CrawlingShabeshSettings
+    {
+        public string SiteAddress { get; set; }
+        public string HtmlTag { get; set; }
+        public string HtmlClassName { get; set; }
+    }
+}
